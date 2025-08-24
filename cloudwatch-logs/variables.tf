3b76f0c18824ae7f8a0ce77f0ad13b6b856b3973
sourabh <sourabh92@gmail.com>
2025-08-24 10:53:54 -0400

    wip

--- cloudwatch-logs/variables.tf
+++ cloudwatch-logs/variables.tf
@@ -1,7 +1,32 @@
-variable "aws_region"     { type = string }
-variable "log_group_name" { type = string }
-variable "retention_days" { type = number  default = 7 }
-variable "kms_key_arn"    { type = string  default = "" }
-variable "app"            { type = string  default = "demo" }
-variable "service"        { type = string  default = "ecs-demo" }
-variable "env"            { type = string  default = "dev" }
+variable "aws_region" {
+  type = string
+}
+
+variable "log_group_name" {
+  type = string
+}
+
+variable "retention_days" {
+  type    = number
+  default = 1
+}
+
+variable "kms_key_arn" {
+  type    = string
+  default = ""
+}
+
+variable "app" {
+  type    = string
+  default = "demo"
+}
+
+variable "service" {
+  type    = string
+  default = "ecs-demo"
+}
+
+variable "env" {
+  type    = string
+  default = "dev"
+}
